b6ead92604587eeefccb6348cea389f67ada0663
pinomad <pinomad@pinomadui-MacBookAir.local>
2023-02-15 13:25:09 +0900

    chore: 변수명 수정

--- services/user.service.js
+++ services/user.service.js
@@ -38,27 +38,45 @@ exports.getSubscribersTrend = async (userId, next) => {
       signInDates.push(value.createdAt);
     });
 
-    const weekDate = [];
+    const weekDates = [];
     for (let i = 0; i <= 7; i += 1) {
       const today = new Date();
-      weekDate.push(new Date(today.setDate(today.getDate() - i)));
+      weekDates.push(new Date(today.setDate(today.getDate() - i)));
     }
 
     const dailyTrends = [0, 0, 0, 0, 0, 0, 0];
     for (let i = 0; i < signInDates.length; i += 1) {
-      if (signInDates[i] >= weekDate[0]) {
+      if (signInDates[i] >= weekDates[0]) {
         dailyTrends[6] += 1;
-      } else if (signInDates[i] < weekDate[0] && signInDates[i] > weekDate[1]) {
+      } else if (
+        signInDates[i] < weekDates[0] &&
+        signInDates[i] > weekDates[1]
+      ) {
         dailyTrends[5] += 1;
-      } else if (signInDates[i] < weekDate[1] && signInDates[i] > weekDate[2]) {
+      } else if (
+        signInDates[i] < weekDates[1] &&
+        signInDates[i] > weekDates[2]
+      ) {
         dailyTrends[4] += 1;
-      } else if (signInDates[i] < weekDate[2] && signInDates[i] > weekDate[3]) {
+      } else if (
+        signInDates[i] < weekDates[2] &&
+        signInDates[i] > weekDates[3]
+      ) {
         dailyTrends[3] += 1;
-      } else if (signInDates[i] < weekDate[3] && signInDates[i] > weekDate[4]) {
+      } else if (
+        signInDates[i] < weekDates[3] &&
+        signInDates[i] > weekDates[4]
+      ) {
         dailyTrends[2] += 1;
-      } else if (signInDates[i] < weekDate[4] && signInDates[i] > weekDate[5]) {
+      } else if (
+        signInDates[i] < weekDates[4] &&
+        signInDates[i] > weekDates[5]
+      ) {
         dailyTrends[1] += 1;
-      } else if (signInDates[i] < weekDate[5] && signInDates[i] > weekDate[6]) {
+      } else if (
+        signInDates[i] < weekDates[5] &&
+        signInDates[i] > weekDates[6]
+      ) {
         dailyTrends[0] += 1;
       }
     }
