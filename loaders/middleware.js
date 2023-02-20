1958970aa428adef7f82083682d03c63331bef9e
pinomad <pinomad@pinomadui-MacBookAir.local>
2023-02-20 14:54:29 +0900

    feat:cors 대응

--- loaders/middleware.js
+++ loaders/middleware.js
@@ -3,10 +3,12 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
+const cors = require('cors');
 
 const { cookieSecret } = require('../config');
 
 module.exports = app => {
+  app.use(cors());
   app.use(
     session({
       resave: false,
