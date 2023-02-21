d67b9507ead82e10c72903a3f8045320d7205d1a
pinomad <pinomad@pinomadui-MacBookAir.local>
2023-02-21 15:59:08 +0900

    feat: 구독자 삭제 api 추가

--- services/user.service.js
+++ services/user.service.js
@@ -85,6 +85,15 @@ exports.addNewSubscribers = async function (userId, newSubscribers) {
   await targetUser.save();
 };
 
+exports.deleteSubscribers = async function (userId, subscribers) {
+  await User.updateOne(
+    { _id: userId },
+    {
+      $pull: { subscribers: { _id: { $in: subscribers } } },
+    },
+  );
+};
+
 exports.addEmailIdToUser = async function (userId, emailId) {
   const targetUser = await User.findById(userId).exec();
 
